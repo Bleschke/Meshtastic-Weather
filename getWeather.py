30412690b460c7f41b79aff4cfc2fbec27f55ecf
Brian Leschke <Bleschke@users.noreply.github.com>
2024-12-22 13:32:20 -0500

    Update getWeather.py
    Updated to send current conditions over Meshtastic

--- getWeather.py
+++ getWeather.py
@@ -1,32 +1,72 @@
 #! python
-# getweather.py - it gets forecast from openweathermap.org website for today and the very next two days
-# USAGE: python getweather.py [localization]
-# XI 2020 Arnold Cytrowski
-import json, requests, sys
+# getWeather.py - gets current weather conditions from openweathermap.org website for and sends it over the Meshtastic Mesh
+# USAGE: python getweather.py 
+# Original Script by: 2020 Arnold Cytrowski, and geeksforgeeks.org
+# Modified for Meshtastic by: 2024 Brian Leschke
 
+import json, requests, sys, os
 
-if len(sys.argv) < 2:
-    print('USAGE: python getweather.py [localization]')
-    exit()
+# Enter you API Key here
+APPID = 'enter API key here'
 
-APPID = '07f4b682ff34ee2cd95a8e16e10de3f7'
-
+# base_url
 location = ' '.join(sys.argv[1:])
-url ='https://api.openweathermap.org/data/2.5/forecast/daily?q=%s&cnt=3&APPID=%s ' % (location,
-APPID)
-response = requests.get(url)
-response.raise_for_status()
+base_url ='"http://api.openweathermap.org/data/2.5/weather?'
 
-weather_data = json.loads(response.text)
+# Enter City Latitude and Longitude
+latitude = 'xx.xxxx'
+longitude = '-xx.xxxx'
 
-w = weather_data['list']
-print(f'Actual weather in {location}:')
-print(w[0]['weather'][0]['main'], '-', w[0]['weather'][0]['description'])
-print()
-print('Tommorow:')
-print(w[1]['weather'][0]['main'], '-', w[1]['weather'][0]['description'])
-print()
-print('The day after tommorow, boi:')
-print(w[2]['weather'][0]['main'], '-', w[2]['weather'][0]['description'])
+# complete_url variable to store
+# complete url address
+complete_url = base_url + "appid=" + api_key + "&lat=" + latitude + "&lon=" + longitude
 
-print('aaand that\'s it')
+# get method of reponse object
+# convert json format into
+# python format data
+x = response.json()
+
+# Now x contains list of nested dictionaries
+# Check the value of "cod" key is equal to
+# "404", means city is found otherwise,
+# city is not found
+if x["cod"] != "404":
+
+    # store the value of "main"
+    # key in variable y
+    y = x["main"]
+
+    # store the value corresponding
+    # to the "temp" key of y
+    current_temperature = y["temp"]
+    fTemp = ((current_temperature - 273.15) * 9/5 + 32)
+
+    # store the value corresponding
+    # to the "pressure" key of y
+    current_pressure = y["pressure"]
+
+    # store the value corresponding
+    # to the "humidity" key of y
+    current_humidity = y["humidity"]
+
+    # store the value of "weather"
+    # key in variable z
+    z = x["weather"]
+
+    # store the value corresponding
+    # to the "description" key at
+    # the 0th index of z
+    weather_description = z[0]["description"]
+
+    current_weather =  ("Current Weather in <your location>: " +
+          "\n Conditions: " + str(weather_description) +
+          "\n Temperature: " + str(round(fTemp, 2)) + " F" +
+          "\n Humidity: " + str(current_humidity) + " %" +
+          "\n Pressure: " + str(current_pressure) + " hPa")
+
+    send_content = " ' " + current_weather + " ' "
+
+    os.system("meshtastic --ch-index 3 --sendtext" + send_content)
+
+else:
+    print(" City Not Found ")
